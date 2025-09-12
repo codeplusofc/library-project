d6ce5a59c7d9571c30dee4050b5e271206c49a2b
GuiRuchell <ruchellguilherme100@gmail.com>
2025-09-12 16:02:06 -0300

    fix: solving problem between branches

--- java/src/Pessoa.java
+++ java/src/Pessoa.java
@@ -1,12 +1,2 @@
-<<<<<<< Updated upstream
-package PACKAGE_NAME;
-
-=======
-<<<<<<< Updated upstream
-//SUPER CLASSE
-//TEM MUITAS PROPRIEDADES, É UMA CLASSE RICA EM PROPRIEDADES!
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 public class Pessoa {
 }
