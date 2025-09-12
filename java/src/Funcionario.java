d9cfe98699f7962ba001d43fe2afe795ba955210
Werner <wernersaboia@gmail.com>
2025-09-12 15:54:25 -0300

    feat: adding classes

--- java/src/Funcionario.java
+++ java/src/Funcionario.java
@@ -1,4 +1,22 @@
+<<<<<<< Updated upstream
 package PACKAGE_NAME;
 
 public class Funcionario {
+=======
+<<<<<<< Updated upstream
+public class Funcionario extends Pessoa {
+
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+=======
+public class Funcionario {
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 }
--- java/src/Pessoa.java
+++ java/src/Pessoa.java
@@ -1,4 +1,12 @@
+<<<<<<< Updated upstream
 package PACKAGE_NAME;
 
+=======
+<<<<<<< Updated upstream
+//SUPER CLASSE
+//TEM MUITAS PROPRIEDADES, É UMA CLASSE RICA EM PROPRIEDADES!
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 public class Pessoa {
 }
