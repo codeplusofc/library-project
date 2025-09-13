d77d2398d45a645c922c0dc747973dc744b1f21c
Werner <wernersaboia@gmail.com>
2025-09-13 17:02:12 -0300

    Added Funcionario and Livro classes using constructors to better understandment

--- java/src/Usuario.java
+++ java/src/Usuario.java
@@ -1,29 +1,11 @@
 public class Usuario extends Pessoa {
     private int matricula;
-    private String email;
-    private String senha;
 
-    public void setMatricula(int matricula) {
+    public Usuario(String nome, int idade, String email, String senha, int matricula) {
+        super(nome, idade, email, senha);
         this.matricula = matricula;
     }
 
-    public int getMatricula() {
-        return matricula;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
+    public int getMatricula() { return matricula; }
+    public void setMatricula(int matricula) { this.matricula = matricula; }
 }
