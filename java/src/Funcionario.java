72aa259c27f7d0658a63397d5413ab301496f89f
Werner <wernersaboia@gmail.com>
2025-09-13 17:02:12 -0300

    Added Funcionario and Livro classes using constructors to better understandment

--- java/src/Funcionario.java
+++ java/src/Funcionario.java
@@ -1,12 +1,15 @@
-<<<<<<< Updated upstream
-package PACKAGE_NAME;
-
-public class Funcionario {
-=======
-<<<<<<< Updated upstream
 public class Funcionario extends Pessoa {
-
     private double salario;
+    private long numeroFuncionario;
+    private String horarioExpediente;
+
+    public Funcionario(String nome, int idade, String email, String senha,
+                       double salario, long numeroFuncionario, String horarioExpediente) {
+        super(nome, idade, email, senha);
+        this.salario = salario;
+        this.numeroFuncionario = numeroFuncionario;
+        this.horarioExpediente = horarioExpediente;
+    }
 
     public double getSalario() {
         return salario;
@@ -15,8 +18,20 @@ public class Funcionario extends Pessoa {
     public void setSalario(double salario) {
         this.salario = salario;
     }
-=======
-public class Funcionario {
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
+    public long getNumeroFuncionario() {
+        return numeroFuncionario;
+    }
+
+    public void setNumeroFuncionario(long numeroFuncionario) {
+        this.numeroFuncionario = numeroFuncionario;
+    }
+
+    public String getHorarioExpediente() {
+        return horarioExpediente;
+    }
+
+    public void setHorarioExpediente(String horarioExpediente) {
+        this.horarioExpediente = horarioExpediente;
+    }
 }
--- java/src/Usuario.java
+++ java/src/Usuario.java
@@ -1,36 +1,11 @@
-public class Usuario {
-    private String nome;
+public class Usuario extends Pessoa {
     private int matricula;
-    private String email;
-    private String senha;
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setMatricula(int matricula) {
+    public Usuario(String nome, int idade, String email, String senha, int matricula) {
+        super(nome, idade, email, senha);
         this.matricula = matricula;
     }
 
-    public int getMatricula() {
-        return matricula;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
+    public int getMatricula() { return matricula; }
+    public void setMatricula(int matricula) { this.matricula = matricula; }
 }
